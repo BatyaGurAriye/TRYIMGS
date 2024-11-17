215773eb8c6737fafc964d583aeffad0ec1835c6
BatyaGurAriye <Batiag@ptr-c.com>
2024-11-17 23:43:48 +0200

    ggj

--- Batya_Gur_Ariye/src/test/java/CalculatorTests.java
+++ Batya_Gur_Ariye/src/test/java/CalculatorTests.java
@@ -1,14 +1,13 @@
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import org.junit.Test;
 
 public class CalculatorTests {
     @Test
     public void testCalculate() throws Exception {
         Calculator calc = new Calculator();
         assertEquals(25.0, calc.calculate("1.0+48.0 - 12.0 *2.0"), 0.1);
-        assertThrows(ArithmeticException.class, () -> calc.calculate("1.0+48.0-12.0*2.0/ 0.0"));
+        assertThrows(Exception.class, () -> calc.calculate("1.0+48.0-12.0*2.0/ 0.0"));
         assertEquals(0.0, calc.calculate(""), 0.1);
         assertEquals(7.0, calc.calculate("7"), 0.1);
         assertEquals(-15.0, calc.calculate("15*-1.0"), 0.1);
--- Batya_Gur_Ariye/src/test/java/EvaluatorTests.java
+++ Batya_Gur_Ariye/src/test/java/EvaluatorTests.java
@@ -1,8 +1,7 @@
-import org.junit.Test;
-
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class EvaluatorTests {
 
@@ -16,7 +15,7 @@ public class EvaluatorTests {
         tokens.add(new NumberToken(-1.5));
 
         Evaluator help = new Evaluator();
-        assertEquals(15.5, help.evaluate(tokens), 0.1);
+        assertEquals(15.5, help.calculationAnExerciseArray(tokens), 0.1);
 
         tokens = new ArrayList<Token>();
         tokens.add(new NumberToken(.5));
@@ -24,7 +23,7 @@ public class EvaluatorTests {
         tokens.add(new NumberToken(2.8));
         tokens.add(new OperatorToken('/'));
         tokens.add(new NumberToken(-1.4));
-        assertEquals(2.5, help.evaluate(tokens), 0.1);
+        assertEquals(2.5, help.calculationAnExerciseArray(tokens), 0.1);
 
     }
 }
