c1350a3055ea8ef8eadb36204b40e6ea81577d36
BatyaGurAriye <Batiag@ptr-c.com>
2024-11-17 23:46:08 +0200

    ggj

--- Batya_Gur_Ariye/src/test/java/ExpressionParserTests.java
+++ Batya_Gur_Ariye/src/test/java/ExpressionParserTests.java
@@ -1,22 +1,21 @@
-import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import org.junit.Test;
 
 public class ExpressionParserTests {
     @Test
     public void testParse() throws Exception {
         ExpressionParser ep = new ExpressionParser();
 
-        assertParsedValues(ep.parse("1+48/12"), Arrays.asList(1.0, '+', 48.0, '/', 12.0));
-        assertParsedValues(ep.parse("1-+48/12"), Arrays.asList(1.0, '-', 48.0, '/', 12.0));
-        assertThrows(Exception.class, () -> ep.parse(".1+48.5/12."));
-        assertThrows(Exception.class, () -> ep.parse("1.+48.5/-12."));
-        assertThrows(Exception.class, () -> ep.parse("-1+48.5/12."));
+        assertParsedValues(ep.convertString2Array("1+48/12"), Arrays.asList(1.0, '+', 48.0, '/', 12.0));
+        assertParsedValues(ep.convertString2Array("1-+48/12"), Arrays.asList(1.0, '-', 48.0, '/', 12.0));
+        assertThrows(Exception.class, () -> ep.convertString2Array(".1+48.5/12."));
+        assertThrows(Exception.class, () -> ep.convertString2Array("1.+48.5/-12."));
+        assertThrows(Exception.class, () -> ep.convertString2Array("-1+48.5/12."));
     }
 
     private void assertParsedValues(List<Token> tokens, List<Object> expectedValues) {
@@ -24,7 +23,8 @@ public class ExpressionParserTests {
                 .map(token -> {
                     if (token instanceof NumberToken) {
                         return ((NumberToken) token).getValue();
-                    } else if (token instanceof OperatorToken) {
+                    }
+                    if (token instanceof OperatorToken) {
                         return ((OperatorToken) token).getValue();
                     } else {
                         throw new IllegalArgumentException("Unknown token type");
