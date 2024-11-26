5479a3ace19f2b30c5f17a02acd6f3caecd27b02
BatyaGurAriye <Batiag@ptr-c.com>
2024-11-26 17:28:51 +0200

    Add3

--- Batya_Gur_Ariye/src/test/java/CalculatorTests.java
+++ Batya_Gur_Ariye/src/test/java/CalculatorTests.java
@@ -1,15 +1,13 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Test;
-import org.junit.function.ThrowingRunnable;
 
 public class CalculatorTests {
     @Test
     public void testCalculate() throws Exception {
         Calculator calc = new Calculator();
         assertEquals(25.0, calc.calculate("1.0+48.0 - 12.0 *2.0"), 0.1);
-        assertThrows(Exception.class, (ThrowingRunnable) () -> calc.calculate("1.0+48.0-12.0*2.0/ 0.0"));
+        assertThrows(Exception.class, () -> calc.calculate("1.0+48.0-12.0*2.0/ 0.0"));
         assertEquals(0.0, calc.calculate(""), 0.1);
         assertEquals(7.0, calc.calculate("7"), 0.1);
         assertEquals(-15.0, calc.calculate("15*-1.0"), 0.1);
